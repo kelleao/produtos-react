5bd240273106d7fb242148539fa58a62ca964e8c
Kel Leao <raquel179@gmail.com>
2023-03-23 17:42:15 -0300

    Criando react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
-import NewProductView from "./views/NewProductView";
-import ProductListView from "./views/ProductListView";
+
+const NewProductView = React.lazy(() => import("./views/NewProductView"));
+const ProductListView = React.lazy(() => import("./views/ProductListView"));
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NewProductView />
-        <ProductListView />
+        <Router>
+          <div>
+            <header>
+              <ul className="link-list">
+                <li>
+                  <Link to={"/"}>Novo</Link>
+                </li>
+                <li>
+                  <Link to={"/list"}>Lista</Link>
+                </li>
+              </ul>
+            </header>
+            <div>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Route
+                  path={"/"}
+                  exact
+                  component={(props) => <NewProductView {...props} />}
+                />
+                <Route
+                  path={"/list"}
+                  component={(props) => <ProductListView {...props} />}
+                />
+              </Suspense>
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
